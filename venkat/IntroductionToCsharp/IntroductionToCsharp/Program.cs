37975dc1e2172893f6edfb6e76ba11129b536687
John Kirch <john.kirch.business@gmail.com>
2017-09-05 00:31:45 -0600

    tutorials

---
+++ venkat/IntroductionToCsharp/IntroductionToCsharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to C# Training");
+        Message();
+    }
+
+    static void Message()
+    {
+        Console.WriteLine("Welcome to C# Training");
+    }
+}
