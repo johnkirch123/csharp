2d7db3af18c9b38d43255d252a90efe82803095d
John Kirch <john.kirch.business@gmail.com>
2017-11-27 21:49:35 -0700

    added csharp tutorial
    Udemy Csharp tutorials from Mosh

---
+++ udemy/Variables/Variables/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "John";
+            bool isWorking = true;
+            var numbers = 4;
+            const float Pi = 3.14f;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(numbers);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine(Pi);
+
+            byte b = 1;
+            int i = b;
+
+            Console.WriteLine(i);
+            
+            try
+            {
+                string numberr = "1234";
+                byte inn = Convert.ToByte(numberr);
+                Console.WriteLine(inn);
+
+                try
+                {
+                    string str = "true";
+                    bool bo = Convert.ToBoolean(str);
+                    Console.WriteLine(bo);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The string cannot be converted to a boolean");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Your number is too big!");
+            }
+        }
+    }
+}
