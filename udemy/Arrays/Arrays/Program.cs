b9cd85601317bf9f0b57cd39c64f0c2b215cd538
John Kirch <john.kirch.business@gmail.com>
2017-12-19 18:01:20 -0700

    Array Testing
    Learning the methods of arrays

---
+++ udemy/Arrays/Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Copy()
+            Console.WriteLine("Effect of Copy()");
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            foreach (var n in another)
+                Console.WriteLine(n);
+
+            // Sort()
+            Console.WriteLine("Effect of Sort()");
+            Array.Sort(numbers);
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Reverse()
+            Array.Reverse(numbers);
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+        }
+    }
+}
