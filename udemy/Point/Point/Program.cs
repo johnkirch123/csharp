b7eb335c907a462299b4543d3819a93a189da37f
John Kirch <john.kirch.business@gmail.com>
2018-03-25 23:53:07 -0600

    params, out, ref
    testing different modifiers and keywords.

---
+++ udemy/Point/Point/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation is null");
+
+            Move(newLocation.x, newLocation.y);
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number = int.Parse("abc");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("conversion failed");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is at ({0}, {1})", point.x, point.y);
+
+            point.Move(200, 200);
+            Console.WriteLine("Point is at ({0}, {1})", point.x, point.y);
+
+            try
+            {
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.x, point.y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error has occured");
+            }
+        }
+    }
+}
