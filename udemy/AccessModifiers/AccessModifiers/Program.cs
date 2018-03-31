555dd3152bac182c7c0866dba3bf4841fd6d6bbd
johnkirch123 <john.kirch.business@gmail.com>
2018-03-31 17:10:51 -0600

    Access Modifiers
    Learning access modifiers, public, private, protected, internal, protected internal. Not complete.

---
+++ udemy/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        public class GoldCustomer : Customer
+        {
+            public void OfferVoucher()
+            {
+                //this. - checking to see available methods
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
