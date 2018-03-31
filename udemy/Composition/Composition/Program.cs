a5088bc03707d5a4c21e9d263e82700d379b4f8e
johnkirch123 <john.kirch.business@gmail.com>
2018-03-31 03:35:46 -0600

    Composition
    Benefits of composition over inheritance

--- udemy/Composition/Composition/Program.cs
+++ udemy/Composition/Composition/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,14 @@ namespace Composition
     {
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
 }
