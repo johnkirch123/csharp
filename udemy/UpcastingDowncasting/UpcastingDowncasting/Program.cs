370dc92f521038cd9989a62a2fa0bd6cae61b8d7
johnkirch123 <john.kirch.business@gmail.com>
2018-03-31 17:37:49 -0600

    Casting
    Upcasting, downcasting, using is and as keywords.

--- udemy/UpcastingDowncasting/UpcastingDowncasting/Program.cs
+++ udemy/UpcastingDowncasting/UpcastingDowncasting/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace UpcastingDowncasting
 {
@@ -6,7 +9,26 @@ namespace UpcastingDowncasting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Shape shape = new Text();
+            Text text = (Text) shape;
+            text.
+
+
+            //Text text = new Text();
+            //Shape shape = text;
+
+            //text.Width = 200;
+            //shape.Width = 100;
+
+            //Console.WriteLine(text.Width);
+
+            //StreamReader reader = new StreamReader(new FileStream());
+            //ArrayList list = new ArrayList();
+            //list.Add(1);
+            //list.Add("John");
+            //list.Add(new Text());
+
+            //var anotherList = new List<Shape>();
         }
     }
 }
