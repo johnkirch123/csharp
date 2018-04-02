058eb2aa43faa81754467aba9646ca768f3eb6ac
johnkirch123 <john.kirch.business@gmail.com>
2018-04-02 11:10:16 -0600

    Boxing/Unboxing
    Testing C# boxing and unboxing.

---
+++ udemy/BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("John");
+            list.Add(DateTime.Today);
+
+            // var number = (int) list[1];
+            var anotherList = new List<int>();
+            anotherList.Add(23);
+
+            var names = new List<string>();
+            names.Add("John");
+        }
+    }
+}
---
+++ udemy/Exercises/Stack/Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        public Stack<object> StackSource = new Stack<object>();
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+                StackSource.Push(obj);
+        }
+
+        public  object Pop()
+        {
+            var obj = StackSource.Pop();
+            if (obj != null)
+                return obj;
+            else
+                return "Nothing in stack";
+        }
+
+        public void Clear()
+        {
+            StackSource.Clear();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
