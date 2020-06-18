6a10e108c924d9133445a36e00a9af7608f25e46
John Kirch <john.kirch.business@gmail.com>
2020-06-18 09:10:22 -0600

    asp.net tut

---
+++ udemy/asp-net-mvc/Test Application/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void MultipleLegs(PetStruct petStruct, PetClass petClass)
+        {
+            petStruct.Legs = petStruct.Legs * 2;
+            petClass.Legs = petClass.Legs * 2;
+
+            Console.WriteLine("Internal Method - A " + petStruct.Type + " has " + petStruct.Legs + " Legs");
+            Console.WriteLine("Internal Method - A " + petClass.Type + " has " + petClass.Legs + " Legs");
+        }
+
+        static void Main(string[] args)
+        {
+            PetStruct dog = new PetStruct();
+            dog.Type = PetType.Dog;
+            dog.HasFur = true;
+            dog.Legs = 4;
+
+            PetClass duck = new PetClass();
+            duck.Type = PetType.Duck;
+            duck.HasFur = false;
+            duck.Legs = 2;
+
+            Console.WriteLine("a " + dog.Type + " has " + dog.Legs + " Legs");
+            Console.WriteLine("a " + duck.Type + " has " + duck.Legs + " Legs");
+
+            MultipleLegs(dog, duck);
+
+            Console.WriteLine("a " + dog.Type + " has " + dog.Legs + " Legs");
+            Console.WriteLine("a " + duck.Type + " has " + duck.Legs + " Legs");
+
+            Console.ReadLine();
+        }
+    }
+
+    class PetClass
+    {
+        public int Legs;
+        public PetType Type;
+        public string Name;
+        public bool HasFur;
+    }
+
+    struct PetStruct
+    {
+        public int Legs;
+        public PetType Type;
+        public string Name;
+        public bool HasFur;
+    }
+
+    enum PetType
+    {
+        Dog, Duck
+    }
+}
---
+++ udemy/asp-net-mvc/Test Application/ProgramFlow/ProgramFlow/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ourNumber = new Random().Next(1, 21);
+            int guessNumber = 0;
+
+            for (int n = 5; n > 0; n--)
+            {
+                guessNumber = getGuess();
+
+                if (guessNumber == ourNumber)
+                {
+                    Console.WriteLine("Correct! Well Done!");
+                    break;
+                }
+                else if (guessNumber != ourNumber && n > 1)
+                {
+                    Console.WriteLine("Oops, sorry you only have {0} guesses left.", n-1);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the correct number is {0}!", ourNumber);
+                }
+            }
+
+            //while (ourNumber != guessNumber)
+            //{
+            //    guessNumber = getGuess();
+
+            //    if (guessNumber == ourNumber)
+            //    {
+            //        Console.WriteLine("Correct! Well Done!");
+            //    }
+            //    else if (guessNumber > ourNumber)
+            //    {
+            //        Console.WriteLine("Opps, sorry your number was too high");
+            //    }
+            //    else if (guessNumber < ourNumber)
+            //    {
+            //        Console.WriteLine("Opps, sorry your number was too low");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Sorry, that is wrong!");
+            //    }
+            //}
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadLine();
+        }
+
+        static int getGuess()
+        {
+            int result = 0;
+
+            Console.WriteLine("Please guess a number between 1 and 20.");
+            String answer = Console.ReadLine();
+            Int32.TryParse(answer, out result);
+            return result;
+        }
+    }
+}
